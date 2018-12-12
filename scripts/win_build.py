ab042402b357b22d1b559eb31e408b01f1961f93
Connor <c.mcg132@gmail.com>
2018-12-12 09:52:06 -0500

    Add win_build.py

---
+++ scripts/win_build.py
@@ -0,0 +1,91 @@
+import sys, os, subprocess, shutil
+
+def exec(command, errorMessage="", showOutput=True):
+    print(command)
+    sys.stdout.flush()
+    result = subprocess.call(command)
+    if (result != 0):
+        if errorMessage is not "":
+            print(errorMessage)
+        sys.exit(1)
+
+SDL2_PATH = os.path.abspath("./OUI/lib/windows/SDL2/")
+SDL2_IMAGE_PATH = os.path.abspath("./OUI/lib/windows/SDL2_image/")
+SDL2_TTF_PATH = os.path.abspath("./OUI/lib/windows/SDL2_ttf/")
+
+def is_num(s):
+  try:
+    int(s)
+    return True
+  except:
+    return False
+
+def find_ms_build():
+    cmd = subprocess.Popen('"%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe" -nologo -latest -property installationPath', shell=True, stdout=subprocess.PIPE)
+    for line in cmd.stdout:
+        path = str(line).replace('b\'', '').replace('\\r\\n\'', '').replace('\\\\', '\\')
+
+    cmd = subprocess.Popen('"%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe" -property installationVersion', shell=True, stdout=subprocess.PIPE)
+    for line in cmd.stdout:
+        version = str(line).split('.')[0].replace('b\'', '')
+        if not is_num(version):
+            print("Unable to find Visual Studio installation version")
+            sys.exit(1)
+
+    return "{}\MSBuild\\{}.0\Bin\MSBuild.exe".format(path, version)
+
+if __name__ == "__main__":
+    
+    exec(["cmake", "--version"],
+        errorMessage="You must install CMake 3.12 or above",
+        showOutput=False
+    )
+
+    
+    
+    print("Generating project with CMake")
+    exec([
+        'cmake',
+        '-G', 'Visual Studio 15 2017 Win64',
+        '-S', '.',
+        '-B', 'build/Windows',
+        "-DSDL2_PATH='{}'".format(SDL2_PATH),
+        "-DSDL2_IMAGE_PATH='{}'".format(SDL2_IMAGE_PATH),
+        "-DSDL2_TTF_PATH='{}'".format(SDL2_TTF_PATH),
+    ], "Could not generate project")
+
+    print("Building project with MSBuild.exe")
+    exec([
+        '{}'.format(find_ms_build()),
+        './build/Windows/ALL_BUILD.vcxproj',
+        '/p:Configuration=Debug',
+        '/p:Platform=x64'
+    ], "Could not build project")
+
+    outputFolder = './build/Windows/Debug'
+
+    print("Copying OUI binaries")
+    for root, dirnames, filenames in os.walk('./build/Windows/OUI/Debug'):
+        for filename in filenames:
+            if filename.endswith('.dll'):
+                filepath = os.path.join(root, filename)
+                print("Copying " + filepath)
+                shutil.copy2(os.path.join(root, filename), outputFolder)
+
+    print("Copying SDL binaries")
+    exclude = ['x86']
+    copyZlib = False
+    for root, dir, filenames in os.walk('./OUI/lib/windows'):
+        dir[:] = [d for d in dir if d not in exclude]
+        for filename in filenames:
+            if filename.endswith('.dll'):
+                filepath = os.path.join(root, filename)
+                print("Copying " + filepath)
+                shutil.copy2(filepath, outputFolder)
+    
+    print("Copying data folder")
+    if os.path.isdir(outputFolder + '/data'):
+        shutil.rmtree(outputFolder + '/data')
+    shutil.copytree('./data', outputFolder + '/data')
+    
+     
--- source/main.cpp
+++ source/main.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include "OUI.h"
 
-#include "libplatform/libplatform.h"
-#include "v8.h"
+// #include "libplatform/libplatform.h"
+// #include "v8.h"
 
-#define _DEBUG
+// #define _DEBUG
 
-void testV8();
+// void testV8();
 
 int main(int argc, char *argv[])
 {
 	try {
-        long long v8Start = oui::currentTimeMillis();
-        testV8();
+        // long long v8Start = oui::currentTimeMillis();
+        // testV8();
+        // std::cout << "V8 took " << (oui::currentTimeMillis() - v8Start) << "ms" << std::endl;
 
 		oui::initialize();
 
-        std::cout << "V8 took " << (oui::currentTimeMillis() - v8Start) << "ms" << std::endl;
-		oui::Context* context = oui::OS->createContext();
+		oui::Context* context = oui::OS()->createContext();
 
         long long ouiStart = oui::currentTimeMillis();
 
@@ -86,6 +86,7 @@ int main(int argc, char *argv[])
 		while(true) {
 
 			if(context->process() == -1) {
+				std::cout << "We lost a window" << std::endl;
 				break;
 			}
 
@@ -96,96 +97,98 @@ int main(int argc, char *argv[])
 	} catch(char* error) {
 		std::cout << "Error: " << error << std::endl;
 	}
+
+	std::cout << "Program successfully ended" << std::endl;
     return 0;
 }
 
-void testV8() {
-    // Initialize V8.
-  v8::V8::InitializeICUDefaultLocation("OUI Runtime");
-  v8::V8::InitializeExternalStartupData("OUI Runtime");
-  std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
-  v8::V8::InitializePlatform(platform.get());
-  v8::V8::Initialize();
+// void testV8() {
+//     // Initialize V8.
+//   v8::V8::InitializeICUDefaultLocation("OUI Runtime");
+//   v8::V8::InitializeExternalStartupData("OUI Runtime");
+//   std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+//   v8::V8::InitializePlatform(platform.get());
+//   v8::V8::Initialize();
 
-  // Create a new Isolate and make it the current one.
-  v8::Isolate::CreateParams create_params;
-  create_params.array_buffer_allocator =
-      v8::ArrayBuffer::Allocator::NewDefaultAllocator();
-  v8::Isolate* isolate = v8::Isolate::New(create_params);
-  {
-    v8::Isolate::Scope isolate_scope(isolate);
+//   // Create a new Isolate and make it the current one.
+//   v8::Isolate::CreateParams create_params;
+//   create_params.array_buffer_allocator =
+//       v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+//   v8::Isolate* isolate = v8::Isolate::New(create_params);
+//   {
+//     v8::Isolate::Scope isolate_scope(isolate);
 
-    // Create a stack-allocated handle scope.
-    v8::HandleScope handle_scope(isolate);
+//     // Create a stack-allocated handle scope.
+//     v8::HandleScope handle_scope(isolate);
 
-    // Create a new context.
-    v8::Local<v8::Context> context = v8::Context::New(isolate);
+//     // Create a new context.
+//     v8::Local<v8::Context> context = v8::Context::New(isolate);
 
-    // Enter the context for compiling and running the hello world script.
-    v8::Context::Scope context_scope(context);
+//     // Enter the context for compiling and running the hello world script.
+//     v8::Context::Scope context_scope(context);
 
-    {
-      // Create a string containing the JavaScript source code.
-      v8::Local<v8::String> source =
-          v8::String::NewFromUtf8(isolate, "'Wow, this is the best tes!'",
-                                  v8::NewStringType::kNormal)
-              .ToLocalChecked();
+//     {
+//       // Create a string containing the JavaScript source code.
+//       v8::Local<v8::String> source =
+//           v8::String::NewFromUtf8(isolate, "'Wow, this is the best tes!'",
+//                                   v8::NewStringType::kNormal)
+//               .ToLocalChecked();
 
-      // Compile the source code.
-      v8::Local<v8::Script> script =
-          v8::Script::Compile(context, source).ToLocalChecked();
+//       // Compile the source code.
+//       v8::Local<v8::Script> script =
+//           v8::Script::Compile(context, source).ToLocalChecked();
 
-      // Run the script to get the result.
-      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+//       // Run the script to get the result.
+//       v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
 
-      // Convert the result to an UTF8 string and print it.
-      v8::String::Utf8Value utf8(isolate, result);
-      printf("%s\n", *utf8);
-    }
+//       // Convert the result to an UTF8 string and print it.
+//       v8::String::Utf8Value utf8(isolate, result);
+//       printf("%s\n", *utf8);
+//     }
 
-    {
-      // Use the JavaScript API to generate a WebAssembly module.
-      //
-      // |bytes| contains the binary format for the following module:
-      //
-      //     (func (export "add") (param i32 i32) (result i32)
-      //       get_local 0
-      //       get_local 1
-      //       i32.add)
-      //
-      const char* csource = R"(
-        let bytes = new Uint8Array([
-          0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x07, 0x01,
-          0x60, 0x02, 0x7f, 0x7f, 0x01, 0x7f, 0x03, 0x02, 0x01, 0x00, 0x07,
-          0x07, 0x01, 0x03, 0x61, 0x64, 0x64, 0x00, 0x00, 0x0a, 0x09, 0x01,
-          0x07, 0x00, 0x20, 0x00, 0x20, 0x01, 0x6a, 0x0b
-        ]);
-        let module = new WebAssembly.Module(bytes);
-        let instance = new WebAssembly.Instance(module);
-        instance.exports.add(3, 4);
-      )";
+//     {
+//       // Use the JavaScript API to generate a WebAssembly module.
+//       //
+//       // |bytes| contains the binary format for the following module:
+//       //
+//       //     (func (export "add") (param i32 i32) (result i32)
+//       //       get_local 0
+//       //       get_local 1
+//       //       i32.add)
+//       //
+//       const char* csource = R"(
+//         let bytes = new Uint8Array([
+//           0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x07, 0x01,
+//           0x60, 0x02, 0x7f, 0x7f, 0x01, 0x7f, 0x03, 0x02, 0x01, 0x00, 0x07,
+//           0x07, 0x01, 0x03, 0x61, 0x64, 0x64, 0x00, 0x00, 0x0a, 0x09, 0x01,
+//           0x07, 0x00, 0x20, 0x00, 0x20, 0x01, 0x6a, 0x0b
+//         ]);
+//         let module = new WebAssembly.Module(bytes);
+//         let instance = new WebAssembly.Instance(module);
+//         instance.exports.add(3, 4);
+//       )";
 
-      // Create a string containing the JavaScript source code.
-      v8::Local<v8::String> source =
-          v8::String::NewFromUtf8(isolate, csource, v8::NewStringType::kNormal)
-              .ToLocalChecked();
+//       // Create a string containing the JavaScript source code.
+//       v8::Local<v8::String> source =
+//           v8::String::NewFromUtf8(isolate, csource, v8::NewStringType::kNormal)
+//               .ToLocalChecked();
 
-      // Compile the source code.
-      v8::Local<v8::Script> script =
-          v8::Script::Compile(context, source).ToLocalChecked();
+//       // Compile the source code.
+//       v8::Local<v8::Script> script =
+//           v8::Script::Compile(context, source).ToLocalChecked();
 
-      // Run the script to get the result.
-      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+//       // Run the script to get the result.
+//       v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
 
-      // Convert the result to a uint32 and print it.
-      uint32_t number = result->Uint32Value(context).ToChecked();
-      printf("3 + 4 = %u\n", number);
-    }
-  }
+//       // Convert the result to a uint32 and print it.
+//       uint32_t number = result->Uint32Value(context).ToChecked();
+//       printf("3 + 4 = %u\n", number);
+//     }
+//   }
 
-  // Dispose the isolate and tear down V8.
-  isolate->Dispose();
-  v8::V8::Dispose();
-  v8::V8::ShutdownPlatform();
-  delete create_params.array_buffer_allocator;
-}
+//   // Dispose the isolate and tear down V8.
+//   isolate->Dispose();
+//   v8::V8::Dispose();
+//   v8::V8::ShutdownPlatform();
+//   delete create_params.array_buffer_allocator;
+// }
