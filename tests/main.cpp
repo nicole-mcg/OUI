fb46980d273a0dddabc2b76b90047a727cceccb7
Connor <c.mcg132@gmail.com>
2018-12-14 11:53:01 -0500

    Separated gtest setup from tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 
+#include "OUI.h"
 #include "gtest/gtest.h"
 
-TEST(sample_test_case, sample_test)
-{
-    EXPECT_EQ(1, 1);
-}
-
+// All test files are found by CMake!
 int main(int argc, char** argv) 
 { 
     testing::InitGoogleTest(&argc, argv); 
